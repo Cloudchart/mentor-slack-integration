fba0906050eede74634d1ceeefe22f811cf9225b
Andrey Peresleguine <peresleguine@gmail.com>
2016-03-25 18:15:46 +0100

    added emoji comparison to reactions collector

--- workers/reactionsCollector.js
+++ workers/reactionsCollector.js
@@ -3,6 +3,22 @@ import { errorMarker } from '../lib'
 import { Channel, Message, Team } from '../models'
 import { UserThemeInsight } from '../models/web_app'
 
+const positiveReactions = [
+  'smile', 'laughing', 'blush', 'smiley', 'relaxed', 'smirk', 'heart_eyes', 'kissing_heart',
+  'kissing_closed_eyes', 'relieved', 'satisfied', 'wink', 'grinning', 'kissing', 'stuck_out_tongue',
+  'kissing_smiling_eyes', 'sweat_smile', 'joy', 'astonished', 'yum', 'sunglasses', '+1', 'thumbsup',
+  'point_up_2', 'point_up', 'clap', 'pray', 'metal', 'simple_smile', 'ok_hand', 'white_check_mark',
+  'heart', 'muscle'
+]
+
+const negativeReactions = [
+  'grin', 'flushed', 'worried', 'frowning', 'anguished', 'open_mouth', 'grimacing', 'confused',
+  'hushed', 'expressionless', 'unamused', 'sweat', 'disappointed_relieved', 'weary', 'pensive',
+  'disappointed', 'confounded', 'fearful', 'cold_sweat', 'persevere', 'cry', 'sob', 'scream',
+  'tired_face', 'angry', 'rage', '-1', 'thumbsdown', 'punch', 'dash'
+]
+
+
 // worker – collects reactions
 //
 export let perform = async (messageId, userThemeInsightId, done) => {
@@ -15,12 +31,24 @@ export let perform = async (messageId, userThemeInsightId, done) => {
       done(null)
     } else {
       if (data.message.reactions) {
-        let userThemeInsight = await UserThemeInsight.findById(userThemeInsightId)
-        // TODO: reactions emoji comparison
-        userThemeInsight.update({ rate: 1 })
-      }
+        let rate = 0
+        let positiveReactionsCounter = 0
+        let negativeReactionsCounter = 0
 
-      done(null, true)
+        data.message.reactions.forEach(reaction => {
+          if (positiveReactions.includes(reaction.name)) {
+            positiveReactionsCounter += reaction.count || 0
+          } else if (negativeReactions.includes(reaction.name)) {
+            negativeReactionsCounter += reaction.count || 0
+          }
+        })
+
+        if (positiveReactionsCounter === negativeReactionsCounter) return done(null, true)
+        const userThemeInsight = await UserThemeInsight.findById(userThemeInsightId)
+        if (positiveReactionsCounter > negativeReactionsCounter) { rate = 1 } else { rate = -1 }
+        await userThemeInsight.update({ rate: rate })
+        done(null, true)
+      }
     }
   })
 }
